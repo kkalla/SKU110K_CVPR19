bb2a6c2c83b2ce3063ffc216d3a92df6c7d68692
kkalla <dkswns333@gmail.com>
2020-05-10 06:48:20 +0000

    Fix bug

--- run_train.sh
+++ run_train.sh
@@ -1,19 +1,33 @@
-python -u object_detector_retinanet/keras_retinanet/bin/train.py \
---snapshot-path=./snapshots \
---tensorboard-dir=./logs \
---weights=./backbone_weights/ResNet-50-model.keras.h5 \
---steps=2000 \
---gpu=6 \
---batch-size=2 \
-csv
+# python -u object_detector_retinanet/keras_retinanet/bin/train.py \
+# --snapshot-path=./snapshots/ARRAY_DETECT_MODEL \
+# --tensorboard-dir=./logs/ARRAY_DETECT_MODEL \
+# --weights=./snapshots/resnet50_csv_11.h5 \
+# --steps=10 \
+# --gpu=2 \
+# --batch-size=1 \
+# --epochs=2 \
+# --image-min-side=1500 \
+# --image-max-side=3000 \
+# --random-transform \
+# csv \
+# --base_dir=../data/array_bbox/images \
+# --annotations=../data/array_bbox/annotations/annotations_train.csv \
+# --val-annotations=../data/array_bbox/annotations/valid.csv \
 
-'''
+########################################################################
+
 python -u object_detector_retinanet/keras_retinanet/bin/train_iou.py \
---weights WEIGHT_FILE \
---snapshot-path=./snapshots \
---tensorboard-dir=./logs \
---steps=2000 \
---gpu=6 \
---batch-size=2 \
-csv
-'''
+--weights ./snapshots/ARRAY_DETECT_MODEL/Sun_May_10_06_34_58_2020/resnet50_csv_02.h5 \
+--snapshot-path=./snapshots/ARRAY_DETECT_MODEL \
+--tensorboard-dir=./logs/ARRAY_DETECT_MODEL \
+--steps=10 \
+--gpu=2 \
+--batch-size=1 \
+--epochs=2 \
+--image-min-side=1500 \
+--image-max-side=3000 \
+--random-transform \
+csv \
+--base_dir=../data/array_bbox/images \
+--annotations=../data/array_bbox/annotations/annotations_train.csv \
+--val-annotations=../data/array_bbox/annotations/valid.csv \
