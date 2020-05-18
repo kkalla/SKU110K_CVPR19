7b9b2283a4257f74a071038d37402422a01571b5
kkalla <dkswns333@gmail.com>
2020-05-18 10:17:46 +0000

    Modify sh script

--- run_predict.sh
+++ run_predict.sh
@@ -1,11 +1,11 @@
 export DATA_PATH=/home/jovyan/work/1_user/dkswns333@agilesoda.ai/dmi_solution/data/array_bbox
 
 python object_detector_retinanet/keras_retinanet/bin/predict.py \
---gpu=1 \
+--gpu=2 \
 --image-min-side=1500 \
 --image-max-side=3000 \
 --save-path=./inference_result/ARRAY_DETECT_MODEL \
 csv \
---base_dir=../data/array_bbox/images \
+--base_dir=../data/array_bbox/images/valid \
 --annotations=../data/array_bbox/annotations/valid.csv \
 "./snapshots/ARRAY_DETECT_MODEL/Mon_May_11_11:17:36_2020/iou_resnet50_csv_10.h5"
--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,20 @@
+train w/ sku110k
+python -u object_detector_retinanet/keras_retinanet/bin/train.py \
+--snapshot-path=./snapshots/BASE \
+--tensorboard-dir=./logs/BASE \
+--weights=.//backbone_weights/ResNet-50-model.keras.h5 \
+--gpu=2 \
+--image-min-side=1500 \
+--image-max-side=3000 \
+--random-transform \
+csv \
+--base_dir=../../datasets/SKU110K_fixed/images \
+--annotations=../../datasets/SKU110K_fixed/annotations_train.csv \
+--val-annotations=../../datasets/SKU110K_fixed/annotations_valid.csv \
+
+########################################################################
+# train ARRAY_DETECT_MODEL
+
 # python -u object_detector_retinanet/keras_retinanet/bin/train.py \
 # --snapshot-path=./snapshots/ARRAY_DETECT_MODEL \
 # --tensorboard-dir=./logs/ARRAY_DETECT_MODEL \
@@ -16,18 +33,18 @@
 
 ########################################################################
 
-python -u object_detector_retinanet/keras_retinanet/bin/train_iou.py \
---weights ./snapshots/ARRAY_DETECT_MODEL/Mon_May_11_01_46_22_2020/resnet50_csv_10.h5 \
---snapshot-path=./snapshots/ARRAY_DETECT_MODEL \
---tensorboard-dir=./logs/ARRAY_DETECT_MODEL \
---steps=10 \
---gpu=1 \
---batch-size=1 \
---epochs=10 \
---image-min-side=1500 \
---image-max-side=3000 \
---random-transform \
-csv \
---base_dir=../data/array_bbox/images \
---annotations=../data/array_bbox/annotations/annotations_train.csv \
---val-annotations=../data/array_bbox/annotations/valid.csv \
+# python -u object_detector_retinanet/keras_retinanet/bin/train_iou.py \
+# --weights ./snapshots/ARRAY_DETECT_MODEL/Mon_May_11_01_46_22_2020/resnet50_csv_10.h5 \
+# --snapshot-path=./snapshots/ARRAY_DETECT_MODEL \
+# --tensorboard-dir=./logs/ARRAY_DETECT_MODEL \
+# --steps=10 \
+# --gpu=1 \
+# --batch-size=1 \
+# --epochs=10 \
+# --image-min-side=1500 \
+# --image-max-side=3000 \
+# --random-transform \
+# csv \
+# --base_dir=../data/array_bbox/images \
+# --annotations=../data/array_bbox/annotations/annotations_train.csv \
+# --val-annotations=../data/array_bbox/annotations/valid.csv \
